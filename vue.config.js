ab8fb4b58d2f86fed2bdbd71ed86d2fa7cc5d26f
Debadeep <31258098+debadeepsen@users.noreply.github.com>
2023-07-14 01:40:03 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/vue-chess/' // TODO: check for prod
+  publicPath: ''
 })
