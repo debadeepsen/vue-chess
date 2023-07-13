0b0d7cc3fe5f96bdd9758bbebe600cc1867269bd
Debadeep <31258098+debadeepsen@users.noreply.github.com>
2023-07-14 01:38:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
-sudo rm -rf dist && npm run build && sudo cp -r dist /var/www/html/vue-chess
+sudo rm -rf dist;
+npm run build;
+sudo rm -rf /var/www/html/vue-chess;
+sudo cp -r dist /var/www/html/vue-chess
